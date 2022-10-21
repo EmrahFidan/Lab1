bc1a2f32a15ed90460db86d74adea904c413e21a
Emrahfidan <201805070@stu.adu.edu.tr>
2022-10-21 22:35:39 +0300

    elements checked in sequence

--- src/polindromeNumber.java
+++ src/polindromeNumber.java
@@ -6,7 +6,7 @@ public class polindromeNumber {
 		
 		while (true) {
 
-			Scanner value = new Scanner(System.in); // kullanıcıdan değer almak için
+			Scanner value = new Scanner(System.in); 
 			System.out.println("write -1 to quit");
 			System.out.print("Enter a 7-digit number:");
 			int n = value.nextInt();
@@ -17,13 +17,25 @@ public class polindromeNumber {
 			String n2 = number + ""; // to find the number of elements in the series
 			int[] arr = new int[n2.length()]; // to find out how many elements my know contains
 
-			if (n == -1) { // çıkış
+			if (n == -1) { // exit
 				System.out.println("Program is terminating");
 				break;
 
 			}
+			else{
+				for (int i = 0; i < arr.length; i++) {  // divide into digits of number
+					remainder = n % 10;
+					arr[i] = remainder;
+					n = n / 10;
+				}
+				int counter = 0;
+				for (int i = 0; i < arr.length; i++) {
+					if (arr[i] == arr[arr.length - 1 - i]) { // elements checkend in sequence
+						counter += 1;
+					}
+				}
+			}
 
-			
 	}
 }
 }
