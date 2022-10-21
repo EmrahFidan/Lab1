3b48bab7647b84d5efd1d52e3efe13d40e1c6d24
Emrahfidan <201805070@stu.adu.edu.tr>
2022-10-21 22:32:54 +0300

    loop added

--- src/polindromeNumber.java
+++ src/polindromeNumber.java
@@ -4,19 +4,26 @@ public class polindromeNumber {
 
 	public static void main(String[] args) {
 		
-		Scanner value = new Scanner(System.in); // kullanıcıdan değer almak için
-		System.out.println("write -1 to quit");
-		System.out.print("Enter a 7-digit number:");
-		int n = value.nextInt();
+		while (true) {
 
-		int remainder;
-		int number = n;
+			Scanner value = new Scanner(System.in); // kullanıcıdan değer almak için
+			System.out.println("write -1 to quit");
+			System.out.print("Enter a 7-digit number:");
+			int n = value.nextInt();
 
-		String n2 = number + ""; // to find the number of elements in the series
-		int[] arr = new int[n2.length()]; // to find out how many elements my know contains
+			int remainder;
+			int number = n;
 
-		
+			String n2 = number + ""; // to find the number of elements in the series
+			int[] arr = new int[n2.length()]; // to find out how many elements my know contains
 
+			if (n == -1) { // çıkış
+				System.out.println("Program is terminating");
+				break;
+
+			}
+
+			
 	}
-
+}
 }
