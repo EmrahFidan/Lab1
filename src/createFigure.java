12c24f09f1d97f1eb6d93468f04c83233a0e558e
Emrahfidan <201805070@stu.adu.edu.tr>
2022-10-21 22:17:33 +0300

    while added

--- src/createFigure.java
+++ src/createFigure.java
@@ -3,72 +3,77 @@ import java.util.Scanner;
 public class createFigure {
 
 	public static void main(String[] args) {
-		int space = 0;
-		Scanner value = new Scanner(System.in);
-		System.out.println("write 'q' to quit");
-		System.out.print("Please enter a positive integer value:");
-		String number = value.nextLine();
+		
+		while (true) {
 
-		System.out.println("Value:"+ number);
-		System.out.println("----------------");
+			int space = 0;
+			// kullanıcıdan değer alma
+			Scanner value = new Scanner(System.in); // kullanıcıdan değer almak için
+			System.out.println("write 'q' to quit");
+			System.out.print("Please enter a positive integer value:");
+			String number = value.nextLine();
 
-		if (number.equals("q")) {
-			System.out.println("Program is terminating");
-			break;
-		}
-		else {
-			int n = Integer.parseInt(number); // değer kalan ksıımda integer olmalı
+			System.out.println("Value:" + number);
+			System.out.println("--------------");
+			// çıkış için "q" komutu
+			if (number.equals("q")) {
+				System.out.println("Program is terminating");
+				break; // kod bitişi
+			} else {
+				int n = Integer.parseInt(number); // değer kalan ksıımda integer olmalı
 
-			for (int i = n; i > 0; i = i - 2) { // Şekilin üst kısmı
-
-				for (int j = 0; j < space; j++) {
-
-					System.out.print(" "); // satırların başındaki boşluklar
-				}
-
-				space += 1;
-				for (int j = 0; j < i; j++) {
-
-					System.out.print("*");
-
-				}
-				System.out.println(); // satır atlamak için
-			}
-			if (n % 2 == 0) { // tek-çift kontrolü (çift olma durumu)
-				space -= 1;
-				for (int i = 2; i <= n; i = i + 2) {
+				for (int i = n; i > 0; i = i - 2) { // Şekilin üst kısmı
 
 					for (int j = 0; j < space; j++) {
 
-						System.out.print(" ");
+						System.out.print(" "); // satırların başındaki boşluklar
 					}
 
-					space -= 1;
-
+					space += 1;
 					for (int j = 0; j < i; j++) {
 
 						System.out.print("*");
 
 					}
-					System.out.println(); 
+					System.out.println(); // satır atlamak için
 				}
-			} else { // tek olma durumu
-				space -= 2;
-				for (int i = 3; i <= n; i = i + 2) {
 
-					for (int j = 0; j < space; j++) {
-
-						System.out.print(" ");
-					}
-
+				if (n % 2 == 0) { // tek-çift kontrolü (çift olma durumu)
 					space -= 1;
+					for (int i = 2; i <= n; i = i + 2) {
 
-					for (int j = 0; j < i; j++) {
+						for (int j = 0; j < space; j++) {
 
-						System.out.print("*");
+							System.out.print(" ");
+						}
 
+						space -= 1;
+
+						for (int j = 0; j < i; j++) {
+
+							System.out.print("*");
+
+						}
+						System.out.println(); 
 					}
-					System.out.println();
+				} else { // tek olma durumu
+					space -= 2;
+					for (int i = 3; i <= n; i = i + 2) {
+
+						for (int j = 0; j < space; j++) {
+
+							System.out.print(" ");
+						}
+
+						space -= 1;
+
+						for (int j = 0; j < i; j++) {
+
+							System.out.print("*");
+
+						}
+						System.out.println();
+					}
 				}
 			}
 		}
