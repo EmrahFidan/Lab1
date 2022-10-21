f9e2ed7bc5871e0cf49572e709598d4e9be53753
Emrahfidan <201805070@stu.adu.edu.tr>
2022-10-21 22:25:10 +0300

    initial polindrome number, added value from user

--- src/polindromeNumber.java
+++ src/polindromeNumber.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
 
 public class polindromeNumber {
 
 	public static void main(String[] args) {
 		
-		
+		Scanner value = new Scanner(System.in); // kullanıcıdan değer almak için
+		System.out.println("write -1 to quit");
+		System.out.print("Enter a 7-digit number:");
+		int n = value.nextInt();
 
 	}
 
